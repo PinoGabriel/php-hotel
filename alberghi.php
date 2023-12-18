3c6936a545e79eddd11f148fa0a17edf0371c9ff
PinoGabriel <pinogabriel01@gmail.com>
2023-12-18 15:54:39 +0100

    feat: write on DOM foreach hotles

--- alberghi.php
+++ alberghi.php
@@ -3,9 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css">
     <title>Document</title>
 </head>
+
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
 <body>
-    <h1>ciao</h1>
+    <div></div>
+    <p class="text-danger"><?php foreach ($hotels as $hotel) {
+        echo "Titolo: " . $hotel['name'] . "<br>" . "Descrizione: " . $hotel['description'] . "<br>" . "Parking: " . $hotel['parking'] . "<br>" . "Vote: " . $hotel['vote'] . "<br>" . "Distanza dal centro: " . $hotel['distance_to_center'] . '<br><br><br>';;
+    }  ?></p>
 </body>
 </html>
